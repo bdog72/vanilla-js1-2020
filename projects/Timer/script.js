055ff21aac596b2ccc859f4859ed9456275c016b
Brian Bycynski <high5bri@hotmail.com>
2020-03-15 06:14:59 -0400

    finished timer project

--- projects/Timer/script.js
+++ projects/Timer/script.js
@@ -1,32 +1,28 @@
 //
 //
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    const timeRemaining = parseFloat(this.durationInput.value);
-    this.durationInput.value = timeRemaining - 1;
-  };
-}
 
 const durationInput = document.querySelector('#duration');
 const startButton1 = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton1, pauseButton);
+const circle = document.querySelector('circle');
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
+
+const timer = new Timer(durationInput, startButton1, pauseButton, {
+  onStart(totalDuration) {
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      'stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+  onComplete() {
+    console.log('Timer is completed');
+  }
+});
