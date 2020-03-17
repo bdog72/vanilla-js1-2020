13dc18d615daa5fbda694f3418820e05ec4cef6d
Brian Bycynski <high5bri@hotmail.com>
2020-03-17 04:57:11 -0400

    added basic search functionality

--- projects/Movie-Fight/script.js
+++ projects/Movie-Fight/script.js
@@ -1,14 +1,30 @@
 //
 //
-// 55902608
-const fetchData = async () => {
+
+const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '55902608',
-      i: 'tt0848228'
+      s: searchTerm
     }
   });
-  console.log(response.data);
+  return response.data.Search;
 };
 
-fetchData();
+const input = document.querySelector('input');
+
+const onInput = async event => {
+  const movies = await fetchData(event.target.value);
+  console.log(movies);
+  for (let movie of movies) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+    document.querySelector('#target').appendChild(div);
+  }
+};
+
+input.addEventListener('input', debounce(onInput, 1000));
